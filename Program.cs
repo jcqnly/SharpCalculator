70db0fbba5b3581d51fb184a646f5e31e602bccf
jackie <jcqnly@gmail.com>
2018-05-12 20:15:47 -0700

    add new dotnet init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SharpCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
