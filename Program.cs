06742c11b07cdc33c7076debdc4ec791539122fb
jackie <jcqnly@gmail.com>
2018-05-15 19:26:07 -0700

    add a method for math choices

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace SharpCalculator
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Calculate 2 numbers!\n");
+      Console.WriteLine("Bestest Calculator Will Calculate");
+      Choices();
+      /*Console.WriteLine("Calculate 2 numbers!\n");
 
       Console.Write("What's the first number?\n");
       string FirstInput = Console.ReadLine();
@@ -42,8 +44,53 @@ namespace SharpCalculator
         case "4":
         Console.Write("The answer is {0}", Answer = FirstNum / SecondNum);
         break;
+
+        default:
+        break;
       }
-      Console.ReadLine();
+      Console.ReadLine();*/
+    }
+    static public int Choices()
+    {
+      Console.WriteLine("What would you like to do? Choose a number\n" +
+                        "1 - add\n" +
+                        "2 - subtract\n" +
+                        "3 - multiply\n" +
+                        "4 - divide\n" +
+                        "5 - exit\n");
+      int selection = int.Parse(Console.ReadLine()); 
+      return selection;
+    }
+    public int WhichChoice()
+    {
+      int selection = Choices();
+
+      switch (selection)
+      {
+        case 1: 
+
+        break;
+
+        case 2:
+
+        break;
+
+        case 3:
+
+        break;
+
+        case 4:
+
+        break;
+
+        case 5:
+
+        break;
+
+        default:
+
+        break;
+      }
     }
   }
 }
